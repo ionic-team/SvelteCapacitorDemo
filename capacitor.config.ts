66899d1d73fab1916e673893b379b98e90d8803d
loganbrade <gamegineerlabs@gmail.com>
2023-03-27 13:53:59 -0700

    Initializing project

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'my-app',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
---
+++ src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+    import { Geolocation } from '@capacitor/geolocation';
+
+    let loc = null;
+    async function getCurrentPosition(){
+        const res = await Geolocation.getCurrentPosition()
+        loc = res
+    }
+</script>
+
+<div>
+    <h1>Geolocation</h1>
+    <p>Your location is:</p>
+    <p>Latitude: {loc?.coords.latitude}</p>
+    <p>Longitude: {loc?.coords.longitude}</p>
+
+    <button on:click={getCurrentPosition}>
+        Get Current Location
+    </button>
+</div>
